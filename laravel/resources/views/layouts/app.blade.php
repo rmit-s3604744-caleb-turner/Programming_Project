0a6772ae2c44800fa08b183609d14b4e9bdb5291
rmit-s3604744-caleb-turner <s3604744@student.rmit.edu.au>
2018-08-13 14:15:25 +1000

    Hierarchy
    Futher restructuring of the hierarchy

---
+++ laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title> BasicTheme </title>
+	</head>
+	
+	<body>
+		@yield('content')
+	</body>
+</html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+Route::get('/search', function () {
+    return view('search');
 });
