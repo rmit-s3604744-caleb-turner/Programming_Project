95f721598ef50140541d267c8442894bc84b767e
rmit-s3541131-Tim-Quin <s3541131@student.rmit.edu.au>
2018-08-14 21:05:10 +1000

    Synced together

--- Buff Movies/Main.php
+++ Buff Movies/Main.php
@@ -14,7 +14,7 @@
 	<ul>
 	
   		<a href="#home" class= "home">Home</a>
-  		<a href="#news"class= "find">Find a Buddy</a>
+  		<a href="Find Buddy.php"class= "find">Find a Buddy</a>
   		<a href="#contact"class= "movies">Movies</a>
   		<a href="#about"class= "forum">Forum</a>
 	</ul>
