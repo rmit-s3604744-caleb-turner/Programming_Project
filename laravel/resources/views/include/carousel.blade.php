4817e93071e1de8a278530186e87b5078d58d84e
rmit-s3541131-Tim-Quin <s3541131@student.rmit.edu.au>
2018-09-05 00:08:47 +1000

    Migrate CSS into laravel

---
+++ laravel/resources/views/include/carousel.blade.php
@@ -0,0 +1,28 @@
+
+
+<div class ="section2"> 
+
+<div class="Mcontainer">
+	<img src="{{ asset('img/MF.jpg') }}  " class="MI">
+<div class= "C1">
+	<p >Mission Impossible Fallout: "Breaks" Records </p>
+
+	</div>
+
+</div>
+
+
+
+
+<div class = "Tcontainer">
+	<img src="{{ asset('img/TM.jpg') }}  " class="TT">
+
+</div>
+
+<div class= "Bcontainer">
+<img src="{{ asset('img/RA.jpg') }}  " class="BU">
+
+
+</div>
+
+</div>
