9c9d17bc7798c70ac20d9662b6056c4f4ea3fc96
rmit-s3541131-Tim-Quin <s3541131@student.rmit.edu.au>
2018-09-10 16:18:12 +1000

    Correct Minor Visual Issues

--- laravel/resources/views/include/carousel.blade.php
+++ laravel/resources/views/include/carousel.blade.php
@@ -16,11 +16,21 @@
 
 <div class = "Tcontainer">
 	<img src="{{ asset('img/TM.jpg') }}  " class="TT">
+	<div class= "C2">
+	<p >Teen Titans Go To The Movies </p>
+
+	</div>
+
 
 </div>
 
 <div class= "Bcontainer">
 <img src="{{ asset('img/RA.jpg') }}  " class="BU">
+	<div class= "C3">
+	<p >Asians That Happen to Be Crazy Rich </p>
+
+	</div>
+
 
 
 </div>
