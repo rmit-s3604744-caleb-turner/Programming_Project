84bd890a7770b027a05c05ea199f6cd7c4adafce
rmit-s3602880-Elroy-Mascarenhas <s3602880@student.rmit.edu.au>
2018-09-11 22:56:34 +1000

    movie description page
    can be used as general layout

---
+++ Buff Movies/MI.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+
+
+<div class = "nav">
+
+	<ul>
+  		<a href="#home" class= "home">Home</a>
+  		<a href="why.php"class= "find">Find a Buddy</a>
+  		<a href="#contact"class= "movies">Movies</a>
+  		<a href="#about"class= "forum">Forum</a>
+	</ul>
+
+</div> 
+
+    
+  </div>
+              
+<div class="Mheading">
+  <h1> Mission Impossible: Fallout </h1> 
+</div>
+
+<div class="Mcontainer">
+	<img src="img/MI.jpg" class = "MI">
+</div>
+
+<div class="Mrating">
+  <h2>Audience Star Rating</h2>
+  <span class="fa fa-star checked"></span>
+  <span class="fa fa-star checked"></span>
+  <span class="fa fa-star checked"></span>
+  <span class="fa fa-star checked"></span>
+  <span class="fa fa-star"></span>
+</div>
+
+<div class="Mdescription">
+
+  <iframe width="500" height="250" src="https://www.youtube.com/embed/XiHiW4N7-bo?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+  <h4>The best intentions often come back to haunt you. MISSION: IMPOSSIBLE - FALLOUT finds Ethan Hunt (Tom Cruise) and his IMF team (Alec Baldwin, Simon Pegg, Ving Rhames) along with some familiar allies (Rebecca Ferguson, Michelle Monaghan) in a race against time after a mission gone wrong. Henry Cavill, Angela Bassett, and Vanessa Kirby also join the dynamic cast with filmmaker Christopher McQuarrie returning to the helm.</h4>
+
+  <h4>Director: Christopher McQuarrie</h4>
+  <h4>Main Cast: Simon Pegg, Michelle Monaghan, Alec Baldwin, Tom Cruise, Rebecca Ferguson, Henry Cavill, Wes Bentley, Angela Bassett, Vanessa Kirby, Sean Harris, Ving Rhames, Frederick Schmidt</h4>
+  <h4>Classification: M (Action violence and coarse language)</h4>
+  <h4>Genre: Action, Adventure</h4>
+  <h4>Release Date: Thursday, 2 August 2018</h4>
+  <h4>Running Time: 147 Mins</h4>
+</div>  
+
+
+<div class="footer">
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-google"></a>
+<a href="#" class="fa fa-linkedin"></a>
+<a href="#" class="fa fa-youtube"></a>
+<a href="#" class="fa fa-instagram"></a>
+</div>
+
+</body>
+</html>
