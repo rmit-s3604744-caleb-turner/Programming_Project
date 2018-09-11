91cca8afb5946b9681812b5f7dccdc94305de48a
rmit-s3541131-Tim-Quin <s3541131@student.rmit.edu.au>
2018-09-12 01:05:02 +1000

    Minor Touches

--- laravel/resources/views/include/navbar.blade.php
+++ laravel/resources/views/include/navbar.blade.php
@@ -24,9 +24,10 @@
 					<ul class="nav navbar-nav navbar-right">
 						<!-- Authentication Links -->
 						@if (Auth::guest())
-							<li><a href="{{ route('login') }}">Login</a></li>
-							<li><a href="{{ route('register') }}">Register</a></li>
+							<li><a href="{{ route('login') }}"class="login">Login</a></li>/
+							<li><a href="{{ route('register') }}"class="signup">SignUp</a></li>
 						@else
+						
 							<li class="dropdown">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
 									{{ Auth::user()->name }} <span class="caret"></span>
@@ -56,6 +57,7 @@
 									</li>
 								</ul>
 							</li>
+						
 						@endif
 						
 					</ul>
