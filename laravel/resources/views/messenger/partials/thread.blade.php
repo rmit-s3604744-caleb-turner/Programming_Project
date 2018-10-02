fb7385eef57b2208795a1df6bf090cbf3ed594e5
rmit-s3604744-caleb-turner <s3604744@student.rmit.edu.au>
2018-10-02 19:58:19 +1000

    Dictionary Fix
    Apparently the live version of laravel treats objects differently, and accepts dictionaries instead.

--- laravel/resources/views/messenger/partials/thread.blade.php
+++ laravel/resources/views/messenger/partials/thread.blade.php
@@ -5,7 +5,7 @@
         <a href="{{ route('messages.show', $thread->id) }}">{{ $thread->subject }}</a>
         ({{ $thread->userUnreadMessagesCount(Auth::id()) }} unread)</h4>
     <p>
-        {{ $thread->latestMessage->body }}
+        {{ $thread->latestMessage["body"]}}
     </p>
     <p>
         <small><strong>Creator:</strong> {{ $thread->creator()->name }}</small>
